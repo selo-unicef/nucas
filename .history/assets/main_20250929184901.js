7ef61bd710b2589509b88224bf22881852570cb9
Lucas Thaynan <34990294+lucasthaynan@users.noreply.github.com>
2025-11-20 12:44:28 -0300

    melhorias feitas (solicitadas)

--- .history/assets/main_20250929184901.js
+++
@@ -1,8 +0,0 @@
-
-
-// dados
-
-// PE, PB e AL: https://docs.google.com/spreadsheets/d/e/2PACX-1vQgdVEXfEdmf6_S-HREgfIyhrFulhEsDs_Dz0C_SQDizqbSzGuWlkdlAvLyVcUlUc4zzDKmRUPVSD33/pub?gid=1024027397&single=true&output=csv
-
-
-// MA e PI: https://docs.google.com/spreadsheets/d/e/2PACX-1vTPqiRFxht-An0D4qkvXOddlecVuv0LIE1gOEEq93MBwYuFgtaa3pfrvg67s0ZhXsEpvMxgaMz77zUn/pub?gid=1024027397&single=true&output=csv
--- .history/assets/main_20250929201856.js
+++
@@ -1,6 +0,0 @@
-
-
-// dados
-
-// PE, PB e AL: https://docs.google.com/spreadsheets/d/e/2PACX-1vQgdVEXfEdmf6_S-HREgfIyhrFulhEsDs_Dz0C_SQDizqbSzGuWlkdlAvLyVcUlUc4zzDKmRUPVSD33/pub?gid=1024027397&single=true&output=csv
-
